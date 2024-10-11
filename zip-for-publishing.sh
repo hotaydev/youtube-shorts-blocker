b8a2012c6e388330095d35e18c9a71f894bd66cb
TaylorHo <taylor@hotay.dev>
2024-10-11 11:18:28 -0300

    fix: packaging script

--- zip-for-publishing.sh
+++ zip-for-publishing.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm youtube-shorts-blocker-v*.zip
+
 VERSION=$(cat ./manifest.json | jq .version | tr -d '"')
 
-zip -r youtube-shorts-blocker-v${VERSION}.zip ./manifest.json ./scripts ./popup ./images/**/*.png ./_locales
+zip -r youtube-shorts-blocker-v${VERSION}.zip ./manifest.json ./scripts ./popup ./images ./_locales
